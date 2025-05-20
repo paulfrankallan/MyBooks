9ab246d54ab1f0f0db9c8a9637a31f7740eef79c
Paul Allan <p.f.allan@gmail.com>
2025-05-20 12:59:29 +0100

    Show UI for a temp hardcoded list of books

--- app/src/main/java/com/paulallan/mybooks/feature/list/BookListScreen.kt
+++ app/src/main/java/com/paulallan/mybooks/feature/list/BookListScreen.kt
@@ -1,13 +1,75 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.paulallan.mybooks.feature.list
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.paulallan.mybooks.R
 import com.paulallan.mybooks.app.theme.MyBooksTheme
 
 @Composable
 fun BookListScreen(modifier: Modifier = Modifier) {
-    // Stub for the book list screen
+    BookListContent()
+}
+
+@Composable
+fun BookListContent(modifier: Modifier = Modifier) {
+    Scaffold(
+        containerColor = Color.Transparent,
+        modifier = modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(R.string.books_title),
+                        style = MaterialTheme.typography.headlineMedium,
+                        color = MaterialTheme.colorScheme.onPrimary
+                    )
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary
+                )
+            )
+        }
+    ) { paddingValues ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.surface)
+                .padding(paddingValues)
+        ) {
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 12.dp)
+            ) {
+                items(
+                    listOf("Book 1", "Book 2", "Book 3", "Book 4", "Book 5", "Book 6", "Book 7", "Book 8", "Book 9", "Book 10")
+                ) { book ->
+                    Text(
+                        text = book,
+                        color = MaterialTheme.colorScheme.onSurface,
+                    )
+                }
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
