eb1a3f561675ab3fb893960e66005c565e49b940
Paul Allan <p.f.allan@gmail.com>
2025-05-20 12:35:34 +0100

    * Some initial UI & DI. * Added internet permission

--- app/src/main/java/com/paulallan/mybooks/app/presentation/AppActivity.kt
+++ app/src/main/java/com/paulallan/mybooks/app/presentation/AppActivity.kt
@@ -6,42 +6,29 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.paulallan.mybooks.app.theme.MyBooksTheme
+import com.paulallan.mybooks.feature.list.BookListScreen
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : ComponentActivity() {
+@AndroidEntryPoint
+class AppActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             MyBooksTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                    Scaffold(
+                        containerColor = MaterialTheme.colorScheme.primary,
+                        modifier = Modifier.Companion.fillMaxSize(),
+                    ) { innerPadding ->
+                        BookListScreen(
+                            modifier = Modifier.Companion.padding(innerPadding)
+                        )
+                    }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyBooksTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/paulallan/mybooks/feature/list/BookListScreen.kt
@@ -0,0 +1,19 @@
+package com.paulallan.mybooks.feature.list
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.paulallan.mybooks.app.theme.MyBooksTheme
+
+@Composable
+fun BookListScreen(modifier: Modifier = Modifier) {
+    // Stub for the book list screen
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BookListScreenPreview() {
+    MyBooksTheme {
+        BookListScreen()
+    }
+}
